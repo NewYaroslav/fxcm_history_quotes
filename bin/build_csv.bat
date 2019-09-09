700d1b4f07f68ae2f8788a7e8b07eef59be2927e
Yaroslav <elektroyar@yandex.ru>
2019-09-09 19:14:03 +0300

    добавлены котировки

---
+++ bin/build_csv.bat
@@ -0,0 +1,50 @@
+ECHO "start!"
+MKDIR "../csv"
+xqhtools convert_storage path_storage "..\storage\AUDCAD.qhs4" path_csv "..\csv\AUDCAD1.csv" -gmtmsk -m4 -sbc
+xqhtools convert_storage path_storage "..\storage\AUDCHF.qhs4" path_csv "..\csv\AUDCHF1.csv" -gmtmsk -m4 -sbc
+xqhtools convert_storage path_storage "..\storage\AUDJPY.qhs4" path_csv "..\csv\AUDJPY1.csv" -gmtmsk -m4 -sbc
+xqhtools convert_storage path_storage "..\storage\AUDNZD.qhs4" path_csv "..\csv\AUDNZD1.csv" -gmtmsk -m4 -sbc
+xqhtools convert_storage path_storage "..\storage\AUDUSD.qhs4" path_csv "..\csv\AUDUSD1.csv" -gmtmsk -m4 -sbc
+xqhtools convert_storage path_storage "..\storage\BTCUSD.qhs4" path_csv "..\csv\BTCUSD1.csv" -gmtmsk -m4 -sbc
+xqhtools convert_storage path_storage "..\storage\CADCHF.qhs4" path_csv "..\csv\CADCHF1.csv" -gmtmsk -m4 -sbc
+xqhtools convert_storage path_storage "..\storage\CADJPY.qhs4" path_csv "..\csv\CADJPY1.csv" -gmtmsk -m4 -sbc
+xqhtools convert_storage path_storage "..\storage\CHFJPY.qhs4" path_csv "..\csv\CHFJPY1.csv" -gmtmsk -m4 -sbc
+xqhtools convert_storage path_storage "..\storage\ETHUSD.qhs4" path_csv "..\csv\ETHUSD1.csv" -gmtmsk -m4 -sbc
+xqhtools convert_storage path_storage "..\storage\EURAUD.qhs4" path_csv "..\csv\EURAUD1.csv" -gmtmsk -m4 -sbc
+xqhtools convert_storage path_storage "..\storage\EURCAD.qhs4" path_csv "..\csv\EURCAD1.csv" -gmtmsk -m4 -sbc
+xqhtools convert_storage path_storage "..\storage\EURCHF.qhs4" path_csv "..\csv\EURCHF1.csv" -gmtmsk -m4 -sbc
+xqhtools convert_storage path_storage "..\storage\EURGBP.qhs4" path_csv "..\csv\EURGBP1.csv" -gmtmsk -m4 -sbc
+xqhtools convert_storage path_storage "..\storage\EURJPY.qhs4" path_csv "..\csv\EURJPY1.csv" -gmtmsk -m4 -sbc
+xqhtools convert_storage path_storage "..\storage\EURNOK.qhs4" path_csv "..\csv\EURNOK1.csv" -gmtmsk -m4 -sbc
+xqhtools convert_storage path_storage "..\storage\EURNZD.qhs4" path_csv "..\csv\EURNZD1.csv" -gmtmsk -m4 -sbc
+xqhtools convert_storage path_storage "..\storage\EURSEK.qhs4" path_csv "..\csv\EURSEK1.csv" -gmtmsk -m4 -sbc
+xqhtools convert_storage path_storage "..\storage\EURTRY.qhs4" path_csv "..\csv\EURTRY1.csv" -gmtmsk -m4 -sbc
+xqhtools convert_storage path_storage "..\storage\EURUSD.qhs4" path_csv "..\csv\EURUSD1.csv" -gmtmsk -m4 -sbc
+xqhtools convert_storage path_storage "..\storage\GBPAUD.qhs4" path_csv "..\csv\GBPAUD1.csv" -gmtmsk -m4 -sbc
+xqhtools convert_storage path_storage "..\storage\GBPCAD.qhs4" path_csv "..\csv\GBPCAD1.csv" -gmtmsk -m4 -sbc
+xqhtools convert_storage path_storage "..\storage\GBPCHF.qhs4" path_csv "..\csv\GBPCHF1.csv" -gmtmsk -m4 -sbc
+xqhtools convert_storage path_storage "..\storage\GBPJPY.qhs4" path_csv "..\csv\GBPJPY1.csv" -gmtmsk -m4 -sbc
+xqhtools convert_storage path_storage "..\storage\GBPNZD.qhs4" path_csv "..\csv\GBPNZD1.csv" -gmtmsk -m4 -sbc
+xqhtools convert_storage path_storage "..\storage\GBPUSD.qhs4" path_csv "..\csv\GBPUSD1.csv" -gmtmsk -m4 -sbc
+xqhtools convert_storage path_storage "..\storage\LTCUSD.qhs4" path_csv "..\csv\LTCUSD1.csv" -gmtmsk -m4 -sbc
+xqhtools convert_storage path_storage "..\storage\NZDCAD.qhs4" path_csv "..\csv\NZDCAD1.csv" -gmtmsk -m4 -sbc
+xqhtools convert_storage path_storage "..\storage\NZDCHF.qhs4" path_csv "..\csv\NZDCHF1.csv" -gmtmsk -m4 -sbc
+xqhtools convert_storage path_storage "..\storage\NZDJPY.qhs4" path_csv "..\csv\NZDJPY1.csv" -gmtmsk -m4 -sbc
+xqhtools convert_storage path_storage "..\storage\NZDUSD.qhs4" path_csv "..\csv\NZDUSD1.csv" -gmtmsk -m4 -sbc
+xqhtools convert_storage path_storage "..\storage\USDCAD.qhs4" path_csv "..\csv\USDCAD1.csv" -gmtmsk -m4 -sbc
+xqhtools convert_storage path_storage "..\storage\USDCHF.qhs4" path_csv "..\csv\USDCHF1.csv" -gmtmsk -m4 -sbc
+xqhtools convert_storage path_storage "..\storage\USDCNH.qhs4" path_csv "..\csv\USDCNH1.csv" -gmtmsk -m4 -sbc
+xqhtools convert_storage path_storage "..\storage\USDHKD.qhs4" path_csv "..\csv\USDHKD1.csv" -gmtmsk -m4 -sbc
+xqhtools convert_storage path_storage "..\storage\USDJPY.qhs4" path_csv "..\csv\USDJPY1.csv" -gmtmsk -m4 -sbc
+xqhtools convert_storage path_storage "..\storage\USDMXN.qhs4" path_csv "..\csv\USDMXN1.csv" -gmtmsk -m4 -sbc
+xqhtools convert_storage path_storage "..\storage\USDNOK.qhs4" path_csv "..\csv\USDNOK1.csv" -gmtmsk -m4 -sbc
+xqhtools convert_storage path_storage "..\storage\USDSEK.qhs4" path_csv "..\csv\USDSEK1.csv" -gmtmsk -m4 -sbc
+xqhtools convert_storage path_storage "..\storage\USDTRY.qhs4" path_csv "..\csv\USDTRY1.csv" -gmtmsk -m4 -sbc
+xqhtools convert_storage path_storage "..\storage\USDZAR.qhs4" path_csv "..\csv\USDZAR1.csv" -gmtmsk -m4 -sbc
+xqhtools convert_storage path_storage "..\storage\XAGUSD.qhs4" path_csv "..\csv\XAGUSD1.csv" -gmtmsk -m4 -sbc
+xqhtools convert_storage path_storage "..\storage\XAUUSD.qhs4" path_csv "..\csv\XAUUSD1.csv" -gmtmsk -m4 -sbc
+xqhtools convert_storage path_storage "..\storage\XRPUSD.qhs4" path_csv "..\csv\XRPUSD1.csv" -gmtmsk -m4 -sbc
+pause
+
+
+
---
+++ bin/build_storage.bat
@@ -0,0 +1,50 @@
+ECHO "start!"
+MKDIR "../storage"
+xqhtools convert_csv path_storage "..\storage\AUDCAD.qhs4" path_csv "..\csv_raw\AUDCAD1.csv" -mskgmt -ohlc -c
+xqhtools convert_csv path_storage "..\storage\AUDCHF.qhs4" path_csv "..\csv_raw\AUDCHF1.csv" -mskgmt -ohlc -c
+xqhtools convert_csv path_storage "..\storage\AUDJPY.qhs4" path_csv "..\csv_raw\AUDJPY1.csv" -mskgmt -ohlc -c
+xqhtools convert_csv path_storage "..\storage\AUDNZD.qhs4" path_csv "..\csv_raw\AUDNZD1.csv" -mskgmt -ohlc -c
+xqhtools convert_csv path_storage "..\storage\AUDUSD.qhs4" path_csv "..\csv_raw\AUDUSD1.csv" -mskgmt -ohlc -c
+xqhtools convert_csv path_storage "..\storage\BTCUSD.qhs4" path_csv "..\csv_raw\BTCUSD1.csv" -mskgmt -ohlc -c
+xqhtools convert_csv path_storage "..\storage\CADCHF.qhs4" path_csv "..\csv_raw\CADCHF1.csv" -mskgmt -ohlc -c
+xqhtools convert_csv path_storage "..\storage\CADJPY.qhs4" path_csv "..\csv_raw\CADJPY1.csv" -mskgmt -ohlc -c
+xqhtools convert_csv path_storage "..\storage\CHFJPY.qhs4" path_csv "..\csv_raw\CHFJPY1.csv" -mskgmt -ohlc -c
+xqhtools convert_csv path_storage "..\storage\ETHUSD.qhs4" path_csv "..\csv_raw\ETHUSD1.csv" -mskgmt -ohlc -c
+xqhtools convert_csv path_storage "..\storage\EURAUD.qhs4" path_csv "..\csv_raw\EURAUD1.csv" -mskgmt -ohlc -c
+xqhtools convert_csv path_storage "..\storage\EURCAD.qhs4" path_csv "..\csv_raw\EURCAD1.csv" -mskgmt -ohlc -c
+xqhtools convert_csv path_storage "..\storage\EURCHF.qhs4" path_csv "..\csv_raw\EURCHF1.csv" -mskgmt -ohlc -c
+xqhtools convert_csv path_storage "..\storage\EURGBP.qhs4" path_csv "..\csv_raw\EURGBP1.csv" -mskgmt -ohlc -c
+xqhtools convert_csv path_storage "..\storage\EURJPY.qhs4" path_csv "..\csv_raw\EURJPY1.csv" -mskgmt -ohlc -c
+xqhtools convert_csv path_storage "..\storage\EURNOK.qhs4" path_csv "..\csv_raw\EURNOK1.csv" -mskgmt -ohlc -c
+xqhtools convert_csv path_storage "..\storage\EURNZD.qhs4" path_csv "..\csv_raw\EURNZD1.csv" -mskgmt -ohlc -c
+xqhtools convert_csv path_storage "..\storage\EURSEK.qhs4" path_csv "..\csv_raw\EURSEK1.csv" -mskgmt -ohlc -c
+xqhtools convert_csv path_storage "..\storage\EURTRY.qhs4" path_csv "..\csv_raw\EURTRY1.csv" -mskgmt -ohlc -c
+xqhtools convert_csv path_storage "..\storage\EURUSD.qhs4" path_csv "..\csv_raw\EURUSD1.csv" -mskgmt -ohlc -c
+xqhtools convert_csv path_storage "..\storage\GBPAUD.qhs4" path_csv "..\csv_raw\GBPAUD1.csv" -mskgmt -ohlc -c
+xqhtools convert_csv path_storage "..\storage\GBPCAD.qhs4" path_csv "..\csv_raw\GBPCAD1.csv" -mskgmt -ohlc -c
+xqhtools convert_csv path_storage "..\storage\GBPCHF.qhs4" path_csv "..\csv_raw\GBPCHF1.csv" -mskgmt -ohlc -c
+xqhtools convert_csv path_storage "..\storage\GBPJPY.qhs4" path_csv "..\csv_raw\GBPJPY1.csv" -mskgmt -ohlc -c
+xqhtools convert_csv path_storage "..\storage\GBPNZD.qhs4" path_csv "..\csv_raw\GBPNZD1.csv" -mskgmt -ohlc -c
+xqhtools convert_csv path_storage "..\storage\GBPUSD.qhs4" path_csv "..\csv_raw\GBPUSD1.csv" -mskgmt -ohlc -c
+xqhtools convert_csv path_storage "..\storage\LTCUSD.qhs4" path_csv "..\csv_raw\LTCUSD1.csv" -mskgmt -ohlc -c
+xqhtools convert_csv path_storage "..\storage\NZDCAD.qhs4" path_csv "..\csv_raw\NZDCAD1.csv" -mskgmt -ohlc -c
+xqhtools convert_csv path_storage "..\storage\NZDCHF.qhs4" path_csv "..\csv_raw\NZDCHF1.csv" -mskgmt -ohlc -c
+xqhtools convert_csv path_storage "..\storage\NZDJPY.qhs4" path_csv "..\csv_raw\NZDJPY1.csv" -mskgmt -ohlc -c
+xqhtools convert_csv path_storage "..\storage\NZDUSD.qhs4" path_csv "..\csv_raw\NZDUSD1.csv" -mskgmt -ohlc -c
+xqhtools convert_csv path_storage "..\storage\USDCAD.qhs4" path_csv "..\csv_raw\USDCAD1.csv" -mskgmt -ohlc -c
+xqhtools convert_csv path_storage "..\storage\USDCHF.qhs4" path_csv "..\csv_raw\USDCHF1.csv" -mskgmt -ohlc -c
+xqhtools convert_csv path_storage "..\storage\USDCNH.qhs4" path_csv "..\csv_raw\USDCNH1.csv" -mskgmt -ohlc -c
+xqhtools convert_csv path_storage "..\storage\USDHKD.qhs4" path_csv "..\csv_raw\USDHKD1.csv" -mskgmt -ohlc -c
+xqhtools convert_csv path_storage "..\storage\USDJPY.qhs4" path_csv "..\csv_raw\USDJPY1.csv" -mskgmt -ohlc -c
+xqhtools convert_csv path_storage "..\storage\USDMXN.qhs4" path_csv "..\csv_raw\USDMXN1.csv" -mskgmt -ohlc -c
+xqhtools convert_csv path_storage "..\storage\USDNOK.qhs4" path_csv "..\csv_raw\USDNOK1.csv" -mskgmt -ohlc -c
+xqhtools convert_csv path_storage "..\storage\USDSEK.qhs4" path_csv "..\csv_raw\USDSEK1.csv" -mskgmt -ohlc -c
+xqhtools convert_csv path_storage "..\storage\USDTRY.qhs4" path_csv "..\csv_raw\USDTRY1.csv" -mskgmt -ohlc -c
+xqhtools convert_csv path_storage "..\storage\USDZAR.qhs4" path_csv "..\csv_raw\USDZAR1.csv" -mskgmt -ohlc -c
+xqhtools convert_csv path_storage "..\storage\XAGUSD.qhs4" path_csv "..\csv_raw\XAGUSD1.csv" -mskgmt -ohlc -c
+xqhtools convert_csv path_storage "..\storage\XAUUSD.qhs4" path_csv "..\csv_raw\XAUUSD1.csv" -mskgmt -ohlc -c
+xqhtools convert_csv path_storage "..\storage\XRPUSD.qhs4" path_csv "..\csv_raw\XRPUSD1.csv" -mskgmt -ohlc -c
+pause
+
+
+
